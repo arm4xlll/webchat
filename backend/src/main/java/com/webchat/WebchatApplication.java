4f08e87be852a0c25be980d3256533f3cd3d0e6d
arm4xlll <109166895+arm4xlll@users.noreply.github.com>
2026-05-31 15:17:47 +0300

    fix: disable open-in-view, exclude UserDetailsServiceAutoConfiguration

--- backend/src/main/java/com/webchat/WebchatApplication.java
+++ backend/src/main/java/com/webchat/WebchatApplication.java
@@ -2,10 +2,11 @@ package com.webchat;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.UserDetailsServiceAutoConfiguration;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = UserDetailsServiceAutoConfiguration.class)
 @EnableAsync
 @EnableScheduling
 public class WebchatApplication {
